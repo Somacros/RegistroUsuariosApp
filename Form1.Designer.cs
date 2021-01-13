19dd8feb4273337415834d41ca49c527224fc56d
Somacros <marcoseduardo2604@gmail.com>
2021-01-13 14:57:20 -0700

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace RegistroUsuarios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.dtpBirth = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnRegistrar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRegistrar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(9)))), ((int)(((byte)(208)))), ((int)(((byte)(14)))));
+            this.btnRegistrar.FlatAppearance.BorderSize = 2;
+            this.btnRegistrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegistrar.Font = new System.Drawing.Font("Microsoft JhengHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRegistrar.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnRegistrar.Location = new System.Drawing.Point(286, 219);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(142, 65);
+            this.btnRegistrar.TabIndex = 0;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = false;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Microsoft JhengHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(286, 94);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(245, 27);
+            this.txtName.TabIndex = 1;
+            // 
+            // dtpBirth
+            // 
+            this.dtpBirth.Font = new System.Drawing.Font("Microsoft JhengHei", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpBirth.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpBirth.Location = new System.Drawing.Point(286, 127);
+            this.dtpBirth.Name = "dtpBirth";
+            this.dtpBirth.Size = new System.Drawing.Size(124, 27);
+            this.dtpBirth.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft JhengHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label1.Location = new System.Drawing.Point(135, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre completo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label2.Location = new System.Drawing.Point(121, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Fecha de nacimiento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft JhengHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label3.Location = new System.Drawing.Point(256, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(200, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Registro de Usuarios";
+            // 
+            // btnClean
+            // 
+            this.btnClean.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnClean.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClean.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.btnClean.FlatAppearance.BorderSize = 2;
+            this.btnClean.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClean.Font = new System.Drawing.Font("Microsoft JhengHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClean.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnClean.Location = new System.Drawing.Point(286, 178);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(142, 35);
+            this.btnClean.TabIndex = 6;
+            this.btnClean.Text = "Limpiar datos";
+            this.btnClean.UseVisualStyleBackColor = false;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.label4.Font = new System.Drawing.Font("Microsoft JhengHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.label4.Location = new System.Drawing.Point(0, 321);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(684, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "*Los usuarios serán almacenados en un archivo de texto con el nombre \"Users\" situ" +
+    "ado en el escritorio del usuario actual.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(46)))), ((int)(((byte)(93)))));
+            this.ClientSize = new System.Drawing.Size(701, 337);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnClean);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtpBirth);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnRegistrar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Registro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.DateTimePicker dtpBirth;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RegistroUsuarios
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            if(txtName.Text != "" && txtName.Text != null)
+            {
+                string messageBack = registerUser(txtName.Text, calculateAge(dtpBirth.Value));
+                MessageBox.Show(messageBack, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            } else
+            {
+                MessageBox.Show("¡Por favor introduzca un nombre en el campo solicitado!","ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private string registerUser(string Name, int age)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string path = desktopPath + @"\Users.txt";
+            if (!File.Exists(path))
+            {
+                using(StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(Name);
+                    sw.WriteLine(age.ToString());
+                    sw.WriteLine("------------------------------------------------------");
+                }
+
+                return String.Format("Nombre: {0}. \nEdad: {1}.\n\nFichero creado con éxito y usuario añadido de manera correcta en {2}.", Name, age, path );
+            } else
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(Name);
+                    sw.WriteLine(age.ToString());
+                    sw.WriteLine("------------------------------------------------------");
+                }
+
+                return String.Format("Nombre: {0}.\nEdad: {1}.\n\nEl nuevo usuario fué agregado al fichero de texto que se encuentra en {2}",Name, age, path);
+            }
+        }
+
+        private int calculateAge(DateTime date)
+        {
+
+            DateTime now = DateTime.Today;
+
+            int age = now.Year - date.Year;
+
+            if (now.Month < date.Month || (now.Month == date.Month && now.Day < date.Day))
+                age--;
+
+            return age;
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            txtName.Text = "";
+            dtpBirth.Value = DateTime.Now;
+        }
+    }
+}
